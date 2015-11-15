8b7b054b22cdf1b95c4be939c5aeafdd7ee1da25
AngelValera <angelvalera.epal@gmail.com>
2015-11-15 20:17:00 +0100

    subiendo

---
+++ merka/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from merka.models import Seccion, Producto
+
+
+# Add in this class to customized the Admin Interface
+class SeccionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('nombre',)}
+
+class ProductoAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('nombre',)}
+
+admin.site.register(Seccion, SeccionAdmin)
+admin.site.register(Producto, ProductoAdmin)
